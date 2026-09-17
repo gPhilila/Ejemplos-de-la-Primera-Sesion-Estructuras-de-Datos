38bdc6f713caa2960f83e8bf1ea828b8d619f14e
phil_ <felixelguay@gmail.com>
2026-09-17 20:47:39 +0200

    Add files via upload

---
+++ Ejemplo09/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void machacal(int x, int* y)
+{
+    *y = x;
+    return;
+}
+
+void machaca2(int x, int& y)
+{
+    y = x;
+    return;
+}
+
+int main(int argc, char** argv)
+{
+    int a, b;
+    cout << "Escribe dos datos enteros:" << endl;
+    cin >> a >> b;
+    
+    int copia = b;
+    cout << "Antes de machaca1: " << a << " y " << b << endl;
+    machacal(a, &b);
+    cout << "Despues de machacal: " << a << " y " << b << endl << endl;
+    
+    b = copia; // restauramos b para probar el segundo método
+    
+    cout << "Antes de machaca2: " << a << " y " << b << endl;
+    machaca2(a, b);
+    cout << "Despues de machaca2: " << a << " y " << b << endl;
+    
+    return 0;
+}
